a94368a3341a9c6192c2d3979ab08f7ebcb90339
Obi-Wan <cayton10@live.marshall.edu>
2020-04-29 00:03:25 -0400

    start comments

--- comments.php
+++ comments.php
@@ -1,63 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Comments</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
-
-  <!-- Favicons -->
-  <link href="img/favicon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700|Roboto:300,400,700&display=swap" rel="stylesheet">
-  <!--GOOGLE FONT-->
-  <link href="https://fonts.googleapis.com/css?family=Baumans&display=swap" rel="stylesheet">
-
-  <!-- Bootstrap CSS File -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="vendor/line-awesome/css/line-awesome.min.css" rel="stylesheet">
-  <link href="vendor/aos/aos.css" rel="stylesheet">
-  <link href="vendor/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="css/style.css" rel="stylesheet">
-  <!-- CUSTOM CSS FILE -->
-  <link href="css/custom.css" rel="stylesheet">
-
-  <!-- =======================================================
-    Template Name: SoftLand
-    Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
-    Author: BootstrapMade.com
-    Author URL: https://bootstrapmade.com/
-  ======================================================= -->
-</head>
-
-<body>
-    
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icofont-close js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-
-    <header>
-      <?
-        require_once("header.php");
-      ?>
-  </header>
-
-
+<?
+require_once('dbConfig/config.php');
+require_once("header.php");
+?>
     <main id="main">
       
         <!-- GRADIENT BACKGROUND AND MESSAGE -->     
